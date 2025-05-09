5e2618d821bbb65c9cc1a4443cb8e2c946c9f078
Congzhi <duzhi02@foxmail.com>
2025-05-09 18:50:47 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Du Zhi on 2025/5/9.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
